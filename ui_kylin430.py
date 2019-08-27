b7419db3e67caecd6b4787c92123a4665e64d959
lzx <lzx@saisi.com>
2019-08-27 18:08:05 +0800

    mulcast uniscast and rate port

--- ui_kylin430.py
+++ ui_kylin430.py
@@ -13,7 +13,7 @@ import ctypes
 
 cfun=CDLL('WinC/adder.so')
 kylin_api = CDLL('kylin_api.so')
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow,  QMessageBox
 from PyQt5 import QtWidgets
@@ -22,9 +22,17 @@ from Ui_ui_kylin430 import Ui_MainWindow
 import time
 import serial
 import serial.tools.list_ports
-import binascii
 import re
 import IPy
+class ClockIdentity(Structure):
+    _fields_ = [
+    ('id', c_uint8 * 8)
+    ]
+class PortIdentity(Structure):
+    _fields_ = [
+    ('clockId',ClockIdentity ), 
+    ('portnum', c_uint16), 
+    ]
 class cmd_reg_t(Structure):
     _fields_ = [
     ('reg', c_int), 
@@ -116,7 +124,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # not use :: is 7    use ::  : num <= 7
         cnt = 7 - ipaddr.count(":")
         cnf_flag = 0
-        j = 0
         sip_type = c_char * 16
         sip_buf = sip_type()
         ipv6_offset = ipaddr.find("::", 0, ipaddr_len)
@@ -129,8 +136,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             sip_list = ipaddr.split(":", ipaddr.count(":"))   
             for i in range(0,ipaddr.count(":") + 1):
                 print(sip_list[i])
-                if i == cnf_flag:
-                    for k in range(0, cnt + 1 ):
+                if i == (cnf_flag + 1):
+                    
+                    for k in range(0, cnt):
                         i = k + i
                         index = 2 * i
                         sip_buf[i] = 0
@@ -140,7 +148,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         print(":: index = " + str(index))
                         #print("%x"%(sip_buf[index]))
                     print(" i = " + str(i))
-                elif i < cnf_flag :
+                elif i <=  cnf_flag :
                     sip_list[i] = "0x" + sip_list[i]
                     tmp = int(sip_list[i], 16)
                     index = i*2
@@ -215,11 +223,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 sip_type = c_uint8 * 16
                 sip_buf = sip_type()
                 sip_buf = self.upack_ipv6(ipaddr)
-                
+                kylin_api.kylin_sip_set.argvtypes = [c_int, POINTER(c_uint8)]
+                kylin_api.kylin_sip_set(0, sip_buf)
             else:
                 print("error")
-
-            
         """
         compile_ip=re.compile('^(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|[1-9])\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)\.(1\d{2}|2[0-4]\d|25[0-5]|[1-9]\d|\d)$')
         if compile_ip.match(ipaddr):
@@ -258,6 +265,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         @param checked DESCRIPTION
         @type bool
         """
+        announce_index = self.comboBox_announce_rate.currentIndex()
+        event_index = self.comboBox_event_rate.currentIndex()
+        cast_type = self.radioButton_unicast.isChecked()
+        kylin_api.kylin_ptp_msg_cast_ctl_set.argvtypes = [c_uint32, c_uint32,  c_uint32 , c_uint32]
+        kylin_api.kylin_ptp_msg_cast_ctl_set(0, cast_type, announce_index, event_index)
+        """
         print("unicast is checked")
         print("cfun" + str(cfun.add_int(1, 2)))
         print("lzx add check clicked")
@@ -270,6 +283,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         print("this is test")
         cfun.test_change_int(test_int_arr_test)
         print("------------" + str(test_int_arr_test[1]))
+        
+        """
+        
+        
+        
         """
         kylin_api.kylin_reg_set.argtypes = [POINTER(cmd_reg_t), c_int]
         cmd_reg_set = cmd_reg_t()
@@ -308,7 +326,158 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         """
         #print(p0)
         self.ptp_stream_type = self.comboBox_stream_type.currentIndex()
+        kylin_api.kylin_ptp_header_set.argvtypes = [c_uint32,c_uint32,c_uint32,c_uint32,c_uint32, ]
+        kylin_api.kylin_ptp_header_set(1, 0, 2, 0, self.ptp_stream_type + 1)
         #print(str(self.ptp_stream_type))
+    @pyqtSlot(str)
+    def on_lineEdit_ClockLevel_textChanged(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        self.lineEdit_ClockLevel.setValidator(QtGui.QIntValidator())
+        text = self.lineEdit_ClockLevel.text()
+        if  text.isdecimal() == False:
+            return
+        clock_level = int (self.lineEdit_ClockLevel.text())
+        kylin_api.kylin_ptp_clocklevel_set.argvtypes = [c_uint32, c_uint32]
+        kylin_api.kylin_ptp_clocklevel_set(0, clock_level)
+        
+
+    @pyqtSlot(str)
+    def on_lineEdit_Priority1_textChanged(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        self.lineEdit_Priority1.setValidator(QtGui.QIntValidator())
+        text = self.lineEdit_Priority1.text()
+        if  text.isdecimal() == False:
+            return
+        priority = int (self.lineEdit_Priority1.text())
+        kylin_api.kylin_ptp_priority1_set.argvtypes = [c_uint32, c_uint32]
+        kylin_api.kylin_ptp_priority1_set(0, priority)
+    
+    @pyqtSlot(str)
+    def on_lineEdit_Priority2_textChanged(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        self.lineEdit_Priority2.setValidator(QtGui.QIntValidator())
+        text = self.lineEdit_Priority2.text()
+        if  text.isdecimal() == False:
+            return
+        priority = int (self.lineEdit_Priority2.text())
+        kylin_api.kylin_ptp_priority2_set.argvtypes = [c_uint32, c_uint32]
+        kylin_api.kylin_ptp_priority2_set(0, priority)
+    
+    @pyqtSlot(str)
+    def on_lineEdit_PortId_textChanged(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        portid_text = self.lineEdit_PortId.text()
+        #portid = re.compile('([1-9|A-F|a-f]{2}:){9}[1-9|A-F|a-f]{2}')
+        portid = re.compile('^([1-9A-Fa-f]{2}:){9}[1-9A-Fa-f]{2}$')
+        
+        if portid.match(portid_text):
+            print("port is correct")
+            portid_type = c_uint8 * 10
+            portid_buf = portid_type()
+            portid_list = portid_text.split(":", portid_text.count(":"))
+            for i in range(0, portid_text.count(":") + 1):
+                portid_list[i] = "0x" + portid_list[i]
+                portid_buf[i] = int(portid_list[i], 16)
+                print(str(int(portid_list[i], 16)))
+                kylin_api.kylin_ptp_local_portId_set.argvtypes = [c_uint32, POINTER(c_uint8)]
+                kylin_api.kylin_ptp_local_portId_set(0, portid_buf)
+        else:
+            print("port is eror")
+            
+    @pyqtSlot(str)
+    def on_lineEdit_sourcePortId_textChanged(self, p0):
+        """
+        Slot documentation goes here.
+        
+        @param p0 DESCRIPTION
+        @type str
+        """
+        portid_text = self.lineEdit_sourcePortId.text()
+        #portid = re.compile('([1-9|A-F|a-f]{2}:){9}[1-9|A-F|a-f]{2}')
+        portid = re.compile('^([1-9A-Fa-f]{2}:){9}[1-9A-Fa-f]{2}$')
+        
+        if portid.match(portid_text):
+            print("port is correct")
+            portid_type = c_uint8 * 10
+            portid_buf = portid_type()
+            portid_list = portid_text.split(":", portid_text.count(":"))
+            for i in range(0, portid_text.count(":") + 1):
+                portid_list[i] = "0x" + portid_list[i]
+                portid_buf[i] = int(portid_list[i], 16)
+                print(str(int(portid_list[i], 16)))
+                portId = PortIdentity()
+                for i in range(0, 7):
+                    portId.clockId.id[i] = portid_buf[i]
+                portId.portnum = (portid_buf[8] << 8) | portid_buf[9]
+                
+                kylin_api.kylin_ptp_bmc_sourceportId_set.argvtypes = [c_uint32, PortIdentity]
+                kylin_api.kylin_ptp_bmc_sourceportId_set(0, portId)
+        else:
+            print("port is eror")
+            
+    @pyqtSlot(int)
+    def on_comboBox_announce_rate_currentIndexChanged(self, index):
+        """
+        Slot documentation goes here.
+        
+        @param index DESCRIPTION
+        @type int
+        """
+        #不确定value的值怎么填
+        announce_index = self.comboBox_announce_rate.currentIndex()
+        event_index = self.comboBox_event_rate.currentIndex()
+        cast_type = self.radioButton_unicast.isChecked()
+        kylin_api.kylin_ptp_msg_cast_ctl_set.argvtypes = [c_uint32, c_uint32,  c_uint32 , c_uint32]
+        kylin_api.kylin_ptp_msg_cast_ctl_set(0, cast_type, announce_index, event_index)
+        
+    
+    @pyqtSlot(int)
+    def on_comboBox_event_rate_currentIndexChanged(self, index):
+        """
+        Slot documentation goes here.
+        
+        @param index DESCRIPTION
+        @type int
+        """
+        announce_index = self.comboBox_announce_rate.currentIndex()
+        event_index = self.comboBox_event_rate.currentIndex()
+        cast_type = self.radioButton_unicast.isChecked()
+        kylin_api.kylin_ptp_msg_cast_ctl_set.argvtypes = [c_uint32, c_uint32,  c_uint32 , c_uint32]
+        kylin_api.kylin_ptp_msg_cast_ctl_set(0, cast_type, announce_index, event_index)
+    
+    @pyqtSlot(bool)
+    def on_radioButton_multicase_clicked(self, checked):
+        """
+        Slot documentation goes here.
+        
+        @param checked DESCRIPTION
+        @type bool
+        """
+        announce_index = self.comboBox_announce_rate.currentIndex()
+        event_index = self.comboBox_event_rate.currentIndex()
+        cast_type = self.radioButton_unicast.isChecked()
+        kylin_api.kylin_ptp_msg_cast_ctl_set.argvtypes = [c_uint32, c_uint32,  c_uint32 , c_uint32]
+        kylin_api.kylin_ptp_msg_cast_ctl_set(0, cast_type, announce_index, event_index)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
@@ -316,4 +485,4 @@ if __name__ == "__main__":
     ui.show()
     sys.exit(app.exec_())
     
-
+    
